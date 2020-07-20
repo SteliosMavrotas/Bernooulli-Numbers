70c55c992f9daf5553df815e7002e3a3a68e5fe6
Stelios Mavrotas <mavrotas.st@gmail.com>
2020-07-20 16:34:07 +0300

    Update BernoulliNumber.py

--- BernoulliNumber.py
+++ BernoulliNumber.py
@@ -2,15 +2,16 @@ from fractions import Fraction
 import sys
 from decimal import Decimal
 def bernoulli_number(n):
+    
     sys.setrecursionlimit(10000) 
     pasc=[[1],[1,1],[1,2,1]]
-    brn=[(float(1),float(1)),(float(-1),float(2))]
+    brn=[Fraction(1),Fraction(-1/2)]
     
     if n%2!=0 and n!=1:
         return 0
     a=calculateBernouli(n,pasc,brn)[-1]
     
-    return Fraction(a[0]/a[1]).limit_denominator(3000)
+    return Fraction(a).limit_denominator()
 
 def calculateBernouli(n,pasc,brn):
     if n==0 or n==1:
@@ -22,16 +23,23 @@ def calculateBernouli(n,pasc,brn):
     if len(pasc)==n+3:
         return brn
     if len(pasc)%2!=0:
-        brn.append((0,0))
+        brn.append(0)
         return calculateBernouli(n, pasc, brn)
     
-    numerator=sum([-1]+[float(-i*y) for i,y in zip([v for c,v in 
-                                                    enumerate(pasc[-1][1:len(pasc[-1])-2]) if brn[c+1][1]!=0],
-                       [Fraction(brn[c+1][0]/brn[c+1][1]).limit_denominator() for c,v in 
-                        enumerate(pasc[-1][1:len(pasc[-1])-2]) if brn[c+1][1]!=0])])
+    '''if len(pasc)==38 and n==36:
+        print(pasc[-1],brn[1:])
+    '''
+    p=[]
+    d=[]
+    for c,v in enumerate(pasc[-1][1:len(pasc[-1])-2]):
+        p.append(v)
+        d.append(brn[c+1])
+    
+    numerator=sum([-1]+[-i*y for i,y in zip(p,d)])
+    
     
     denominator=int(pasc[-1][-2])
     
-    brn.append(float.as_integer_ratio(numerator/denominator))
+    brn.append(Fraction(numerator/denominator))
     
     return calculateBernouli(n, pasc, brn)
